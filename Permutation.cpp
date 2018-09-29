48154d1d964507e4d744daf0e3c9362039e378ac
olcaytaner <olcaytaner@gmail.com>
2018-09-29 20:20:50 +0300

    Added Permutation class

---
+++ Permutation.cpp
@@ -0,0 +1,66 @@
+//
+// Created by Olcay Taner Yıldız on 29.09.2018.
+//
+
+#include "Permutation.h"
+
+/**
+ * A constructor of {@link Permutation} class which creates a new array and assigns integer
+ * numbers starting from 0 to given input n.
+ *
+ * @param n integer input.
+ */
+Permutation::Permutation(int n) {
+    int i;
+    this->n = n;
+    a = new int[n];
+    for (i = 0; i < n; i++) {
+        a[i] = i;
+    }
+}
+
+Permutation::~Permutation() {
+    delete a;
+}
+
+/**
+ * The get method returns the array a.
+ *
+ * @return Array a.
+ */
+int* Permutation::get(){
+    return a;
+}
+
+/**
+ * The next method generates next permutation for the array a.
+ *
+ * @return true if next permutation is possible, false otherwise.
+ */
+bool Permutation::next() {
+    int i = n - 2;
+    int j, tmp, k;
+    while (i >= 0 && a[i] >= a[i + 1]) {
+        i--;
+    }
+    if (i == -1) {
+        return false;
+    }
+    j = n - 1;
+    while (a[i] >= a[j]) {
+        j--;
+    }
+    tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
+    k = i + 1;
+    j = n - 1;
+    while (k < j) {
+        tmp = a[k];
+        a[k] = a[j];
+        a[j] = tmp;
+        k++;
+        j--;
+    }
+    return true;
+}
---
+++ Permutation.h
@@ -0,0 +1,21 @@
+//
+// Created by Olcay Taner Yıldız on 29.09.2018.
+//
+
+#ifndef UTIL_PERMUTATION_H
+#define UTIL_PERMUTATION_H
+
+
+class Permutation {
+private:
+    int n;
+    int* a;
+public:
+    Permutation(int n);
+    int* get();
+    virtual ~Permutation();
+    bool next();
+};
+
+
+#endif //UTIL_PERMUTATION_H
